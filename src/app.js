cd42638f22e083024843740bae0d0ac6085b0bb7
Amy Dang <kirontoo@gmail.com>
2020-11-01 12:27:08 -0800

    set up express

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require( 'express' );
+const morgan = require( 'morgan' );
+const compression = require( 'compression' );
+const helmet = require( 'helmet' );
+
+const app = express();
+app.use( morgan( 'tiny' ));
+app.use( compression() );
+app.use( helmet() );
+
+app.get( '/', ( req, res ) => {
+	res.json({
+		message: 'Home Inventory API'
+	});
+});
+
+module.exports = app;
