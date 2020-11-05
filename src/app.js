81913a08abb08aa55b8fcbc9152f39b57655629e
Amy Dang <kirontoo@gmail.com>
2020-11-05 15:39:39 -0800

    set up error handling and jest

--- src/app.js
+++ src/app.js
@@ -2,16 +2,23 @@ const express = require( 'express' );
 const morgan = require( 'morgan' );
 const compression = require( 'compression' );
 const helmet = require( 'helmet' );
+const middlewares = require( './middlewares' );
 
 const app = express();
 app.use( morgan( 'tiny' ));
 app.use( compression() );
 app.use( helmet() );
 
+// body parsing
+app.use( express.json() );
+
 app.get( '/', ( req, res ) => {
 	res.json({
 		message: 'Home Inventory API'
 	});
 });
+
+app.use( middlewares.notFound );
+app.use( middlewares.errorHandler );
 
 module.exports = app;
---
+++ src/app.test.js
@@ -0,0 +1,13 @@
+const supertest = require( 'supertest' );
+const app = require( './app' );
+
+describe( 'GET /', () => {
+	it( 'should respond with a message', async () => {
+		const response = await supertest( app )
+			.get( '/' )
+			.expect( 'Content-Type', /json/ )
+			.expect( 200 );
+
+		expect( response.body.message ).toEqual( 'Home Inventory API' );
+	});
+});
