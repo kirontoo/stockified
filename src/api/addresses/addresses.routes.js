cf2da3ceadbd5321727f74be865a2474c9086f81
Amy Dang <kirontoo@gmail.com>
2020-11-14 17:39:06 -0800

    feat: Validate data before db insertion
    We want to only insert unique addresses

--- src/api/addresses/addresses.routes.js
+++ src/api/addresses/addresses.routes.js
@@ -4,7 +4,6 @@ const Address = require( './addresses.model' );
 const router = express.Router();
 
 router.get( '/', async ( req, res, next ) => {
-
 	try {
 		const addresses = await Address
 			.query()
@@ -18,9 +17,23 @@ router.get( '/', async ( req, res, next ) => {
 
 router.post( '/', async ( req, res, next ) => {
 	try {
+		[
+			'street_address_1',
+			'street_address_2',
+			'city',
+			'zipcode'
+		].forEach( ( prop ) => {
+			if ( req.body[ prop ] )
+				req.body[ prop ] = req.body[ prop ]
+					.toString()
+					.toLowerCase()
+					.trim();
+		});
+
 		const address = await Address
 			.query()
 			.insert( req.body );
+
 		return res.json( address );
 	} catch( error ) {
 		next( error );
--- src/api/addresses/addresses.test.js
+++ src/api/addresses/addresses.test.js
@@ -10,7 +10,9 @@ describe( 'GET /api/v1/addresses', () => {
 
 		expect( response.body.length ).toEqual( 0 );
 	});
+});
 
+describe( 'POST /api/v1/addresses', () => {
 	it( 'should insert a address', async () => {
 		const address = {
 			street_address_1: '1234 walker st.',
@@ -26,9 +28,40 @@ describe( 'GET /api/v1/addresses', () => {
 			.expect( 'Content-Type', /json/ )
 			.expect( 200 );
 
-		expect( response.body ).toEqual( address );
+		expect( response.body ).toEqual( 
+			expect.objectContaining({
+				...address,
+				id: 1
+			}));
 	});
 
 	it( 'should fail to insert an address', async () => {
+		const address = {
+			street_address_1: '1234 walker st.',
+			city: 'santa ana',
+			state_id: 5
+		}
+
+		await supertest( app )
+			.post( '/api/v1/addresses' )
+			.send( address )
+			.expect( 'Content-Type', /json/ )
+			.expect( 422 );
+	});
+
+	it( 'should return a validation error on duplicate address', async () => {
+		const address = {
+			street_address_1: '1234 walker st.',
+			city: 'santa ana',
+			zipcode: '82358-7345',
+			state_id: 5,
+			country_id: 236
+		}
+
+		await supertest( app )
+			.post( '/api/v1/addresses' )
+			.send( address )
+			.expect( 'Content-Type', /json/ )
+			.expect( 409 );
 	});
 });
