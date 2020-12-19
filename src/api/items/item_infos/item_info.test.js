322ade28f1ab84701a661c0eaf48bcec71536020
Amy Dang <kirontoo@gmail.com>
2020-12-19 14:06:44 -0800

    feat: Update test for item_info

--- src/api/items/item_infos/item_info.test.js
+++ src/api/items/item_infos/item_info.test.js
@@ -46,10 +46,17 @@ describe( 'POST /api/v1/items/:id/item_infos', () => {
 	// TODO: do I need this?
 	it( 'should fail to create a new item_info', async ( done ) => {
 		const response = await supertest( app )
-			.post( `/api/v1/items/${item_info.item_id}/item_infos` )
-			.send( item_info )
+			.post( `/api/v1/items/4/item_infos` )
+			.send({
+				item_id: 4,
+				user_id: 1,
+				purchase_date: "2020-08-24",
+				inventory_location_id: 2,
+				msrp: 0.75,
+				expiration_date: "2025-01-01"
+			})
 			.expect( 'Content-Type', /json/ )
-			.expect( 422 );
+			.expect( 500 );
 
 		done();
 	});
